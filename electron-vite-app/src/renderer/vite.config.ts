482f1ed5611469c3aa3662afb19af8e19221556e
Joey <786678257@qq.com>
2022-12-13 20:27:53 +0800

    feat: csp config

--- electron-vite-app/src/renderer/vite.config.ts
+++ electron-vite-app/src/renderer/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { builtinModules } from 'module'
 import * as path from 'path'
@@ -7,10 +7,22 @@ import * as path from 'path'
 const ROOT = path.resolve(__dirname, '../../')
 const CHROME_VERSION = 108
 
+const cspMate = `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">`
+
+const htmlPlugin: () => PluginOption = () => {
+  return {
+    name: 'html-transform',
+    apply: 'build',
+    transformIndexHtml(html: string) {
+      return html.replace(/<title>/, `${cspMate}\n\t\t<title>`)
+    },
+  }
+}
+
 export default defineConfig({
   root: __dirname,
   base: './',
-  plugins: [react()],
+  plugins: [react(), htmlPlugin()],
   build: {
     target: `chrome${CHROME_VERSION}`,
     outDir: path.join(ROOT, 'dist/renderer'),
@@ -18,9 +30,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.join(__dirname, 'index.html'),
-        nested: path.join(__dirname, 'about.html')
+        nested: path.join(__dirname, 'about.html'),
       },
       external: ['electron', ...builtinModules],
     },
-  }
+  },
 })
